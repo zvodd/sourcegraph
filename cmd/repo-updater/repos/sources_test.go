99aa95653b666c807d0f276baf9bd48880da0396
Thorsten Ball <mrnugget@gmail.com>
2019-05-08 09:31:42 +0200

    repo-updater: Fix CI by adding AWS CodeCommit region

--- cmd/repo-updater/repos/sources_test.go
+++ cmd/repo-updater/repos/sources_test.go
@@ -192,7 +192,7 @@ func TestSources_ListRepos(t *testing.T) {
 				Config: marshalJSON(t, &schema.AWSCodeCommitConnection{
 					AccessKeyID:     os.Getenv("AWSCODECOMMIT_ACCESS_KEY_ID"),
 					SecretAccessKey: os.Getenv("AWSCODECOMMIT_SECRET_ACCESS_KEY"),
-					Region:          os.Getenv("AWSCODECOMMIT_REGION"),
+					Region:          "us-west-1",
 				}),
 			},
 			{
@@ -290,7 +290,7 @@ func TestSources_ListRepos(t *testing.T) {
 				Config: marshalJSON(t, &schema.AWSCodeCommitConnection{
 					AccessKeyID:     os.Getenv("AWSCODECOMMIT_ACCESS_KEY_ID"),
 					SecretAccessKey: os.Getenv("AWSCODECOMMIT_SECRET_ACCESS_KEY"),
-					Region:          os.Getenv("AWSCODECOMMIT_REGION"),
+					Region:          "us-west-1",
 					Exclude: []*schema.ExcludedAWSCodeCommitRepo{
 						{Name: "stRIPE-gO"},
 						{Id: "020a4751-0f46-4e19-82bf-07d0989b67dd"},                // ID of `test`
@@ -429,7 +429,7 @@ func TestSources_ListRepos(t *testing.T) {
 				Config: marshalJSON(t, &schema.AWSCodeCommitConnection{
 					AccessKeyID:     os.Getenv("AWSCODECOMMIT_ACCESS_KEY_ID"),
 					SecretAccessKey: os.Getenv("AWSCODECOMMIT_SECRET_ACCESS_KEY"),
-					Region:          os.Getenv("AWSCODECOMMIT_REGION"),
+					Region:          "us-west-1",
 				}),
 			},
 		}
@@ -524,7 +524,7 @@ func TestSources_ListRepos(t *testing.T) {
 				Config: marshalJSON(t, &schema.AWSCodeCommitConnection{
 					AccessKeyID:           os.Getenv("AWSCODECOMMIT_ACCESS_KEY_ID"),
 					SecretAccessKey:       os.Getenv("AWSCODECOMMIT_SECRET_ACCESS_KEY"),
-					Region:                os.Getenv("AWSCODECOMMIT_REGION"),
+					Region:                "us-west-1",
 					RepositoryPathPattern: "a/b/c/{name}",
 				}),
 			},

a76f185403cdab5c42bed5c17ff103010c36f056
Loïc Guychard <loic@sourcegraph.com>
2020-06-12 11:38:08 +0200

    convert UserSettingsCreateAccessTokenPage to function component

--- web/src/user/settings/accessTokens/UserSettingsCreateAccessTokenPage.tsx
+++ web/src/user/settings/accessTokens/UserSettingsCreateAccessTokenPage.tsx
@@ -1,9 +1,9 @@
 import { LoadingSpinner } from '@sourcegraph/react-loading-spinner'
 import AddIcon from 'mdi-react/AddIcon'
-import * as React from 'react'
+import React, { useEffect, useCallback, useState, ChangeEvent } from 'react'
 import { RouteComponentProps } from 'react-router'
 import { Link } from 'react-router-dom'
-import { concat, Observable, Subject, Subscription } from 'rxjs'
+import { Observable, merge, of } from 'rxjs'
 import { catchError, concatMap, map, tap } from 'rxjs/operators'
 import { gql } from '../../../../../shared/src/graphql/graphql'
 import * as GQL from '../../../../../shared/src/graphql/schema'
@@ -16,6 +16,7 @@ import { SiteAdminAlert } from '../../../site-admin/SiteAdminAlert'
 import { eventLogger } from '../../../tracking/eventLogger'
 import { UserAreaRouteContext } from '../../area/UserArea'
 import { ErrorAlert } from '../../../components/alerts'
+import { useEventObservable } from '../../../../../shared/src/util/useObservable'
 
 function createAccessToken(user: GQL.ID, scopes: string[], note: string): Observable<GQL.ICreateAccessTokenResult> {
     return mutateGraphQL(
@@ -45,180 +46,153 @@ interface Props extends UserAreaRouteContext, RouteComponentProps<{}> {
     onDidCreateAccessToken: (result: GQL.ICreateAccessTokenResult) => void
 }
 
-interface State {
-    /** The contents of the note input field. */
-    note: string
-
-    /** The selected scopes checkboxes. */
-    scopes: string[]
-
-    creationOrError?: 'loading' | GQL.ICreateAccessTokenResult | ErrorLike
-}
-
 /**
  * A page with a form to create an access token for a user.
  */
-export class UserSettingsCreateAccessTokenPage extends React.PureComponent<Props, State> {
-    public state: State = {
-        note: '',
-        scopes: [AccessTokenScopes.UserAll],
-    }
-
-    private submits = new Subject<React.FormEvent<HTMLFormElement>>()
-    private componentUpdates = new Subject<Props>()
-    private subscriptions = new Subscription()
-
-    public componentDidMount(): void {
+export const UserSettingsCreateAccessTokenPage: React.FunctionComponent<Props> = props => {
+    useEffect(() => {
         eventLogger.logViewEvent('NewAccessToken')
-        this.subscriptions.add(
-            this.submits
-                .pipe(
-                    tap(event => event.preventDefault()),
+    }, [])
+
+    const [note, setNote] = useState('')
+    const onNoteChange: React.ChangeEventHandler<HTMLInputElement> = useCallback(
+        (event: ChangeEvent<HTMLInputElement>) => setNote(event.currentTarget.value),
+        [setNote]
+    )
+
+    const [scopes, setScopes] = useState<string[]>([])
+    const onScopesChange = useCallback(
+        (event: React.ChangeEvent<HTMLInputElement>) => {
+            const checked = event.currentTarget.checked
+            const value = event.currentTarget.value
+            setScopes(checked ? [...scopes, value] : scopes.filter(scope => scope !== value))
+        },
+        [scopes, setScopes]
+    )
+
+    const [nextCreation, creationOrError] = useEventObservable(
+        useCallback(
+            (creations: Observable<void>): Observable<'loading' | GQL.ICreateAccessTokenResult | ErrorLike> =>
+                creations.pipe(
                     concatMap(() =>
-                        concat(
-                            [{ creationOrError: 'loading' }],
-                            createAccessToken(this.props.user.id, this.state.scopes, this.state.note).pipe(
+                        merge(
+                            of('loading' as const),
+                            createAccessToken(props.user.id, scopes, note).pipe(
                                 tap(result => {
                                     // Go back to access tokens list page and display the token secret value.
-                                    this.props.history.push(`${this.props.match.url.replace(/\/new$/, '')}`)
-                                    this.props.onDidCreateAccessToken(result)
+                                    props.history.push(`${props.match.url.replace(/\/new$/, '')}`)
+                                    props.onDidCreateAccessToken(result)
                                 }),
-                                map(result => ({ creationOrError: result })),
-                                catchError(error => [{ creationOrError: asError(error) }])
+                                catchError(error => [asError(error)])
                             )
                         )
                     )
-                )
-                .subscribe(
-                    stateUpdate => this.setState(stateUpdate as State),
-                    error => console.error(error)
-                )
+                ),
+            [props, note, scopes]
         )
+    )
 
-        this.componentUpdates.next(this.props)
-    }
+    const onSubmit = useCallback(
+        (event: React.FormEvent) => {
+            event.preventDefault()
+            nextCreation()
+        },
+        [nextCreation]
+    )
 
-    public componentDidUpdate(): void {
-        this.componentUpdates.next(this.props)
-    }
-
-    public componentWillUnmount(): void {
-        this.subscriptions.unsubscribe()
-    }
-
-    public render(): JSX.Element | null {
-        const siteAdminViewingOtherUser =
-            this.props.authenticatedUser && this.props.authenticatedUser.id !== this.props.user.id
-        return (
-            <div className="user-settings-create-access-token-page">
-                <PageTitle title="Create access token" />
-                <h2>New access token</h2>
-                {siteAdminViewingOtherUser && (
-                    <SiteAdminAlert className="sidebar__alert">
-                        Creating access token for other user <strong>{this.props.user.username}</strong>
-                    </SiteAdminAlert>
-                )}
-                <Form onSubmit={this.onSubmit}>
-                    <div className="form-group">
-                        <label htmlFor="user-settings-create-access-token-page__note">Token description</label>
-                        <input
-                            type="text"
-                            className="form-control e2e-create-access-token-description"
-                            id="user-settings-create-access-token-page__note"
-                            onChange={this.onNoteChange}
-                            required={true}
-                            autoFocus={true}
-                            placeholder="Description"
-                        />
-                        <small className="form-help text-muted">What's this token for?</small>
+    const siteAdminViewingOtherUser = props.authenticatedUser && props.authenticatedUser.id !== props.user.id
+    return (
+        <div className="user-settings-create-access-token-page">
+            <PageTitle title="Create access token" />
+            <h2>New access token</h2>
+            {siteAdminViewingOtherUser && (
+                <SiteAdminAlert className="sidebar__alert">
+                    Creating access token for other user <strong>{props.user.username}</strong>
+                </SiteAdminAlert>
+            )}
+            <Form onSubmit={onSubmit}>
+                <div className="form-group">
+                    <label htmlFor="user-settings-create-access-token-page__note">Token description</label>
+                    <input
+                        type="text"
+                        className="form-control e2e-create-access-token-description"
+                        id="user-settings-create-access-token-page__note"
+                        onChange={onNoteChange}
+                        required={true}
+                        autoFocus={true}
+                        placeholder="Description"
+                    />
+                    <small className="form-help text-muted">What's this token for?</small>
+                </div>
+                <div className="form-group">
+                    <label className="mb-1" htmlFor="user-settings-create-access-token-page__note">
+                        Token scope
+                    </label>
+                    <div>
+                        <small className="form-help text-muted">
+                            Tokens with limited user scopes are not yet supported.
+                        </small>
                     </div>
-                    <div className="form-group">
-                        <label className="mb-1" htmlFor="user-settings-create-access-token-page__note">
-                            Token scope
+                    <div className="form-check">
+                        <input
+                            className="form-check-input"
+                            type="checkbox"
+                            id="user-settings-create-access-token-page__scope-user:all"
+                            checked={true}
+                            value={AccessTokenScopes.UserAll}
+                            onChange={onScopesChange}
+                            disabled={true}
+                        />
+                        <label
+                            className="form-check-label"
+                            htmlFor="user-settings-create-access-token-page__scope-user:all"
+                        >
+                            <strong>{AccessTokenScopes.UserAll}</strong> — Full control of all resources accessible to
+                            the user account
                         </label>
-                        <div>
-                            <small className="form-help text-muted">
-                                Tokens with limited user scopes are not yet supported.
-                            </small>
-                        </div>
+                    </div>
+                    {props.user.siteAdmin && (
                         <div className="form-check">
                             <input
                                 className="form-check-input"
                                 type="checkbox"
-                                id="user-settings-create-access-token-page__scope-user:all"
-                                checked={true}
-                                value={AccessTokenScopes.UserAll}
-                                onChange={this.onScopesChange}
-                                disabled={true}
+                                id="user-settings-create-access-token-page__scope-site-admin:sudo"
+                                checked={scopes.includes(AccessTokenScopes.SiteAdminSudo)}
+                                value={AccessTokenScopes.SiteAdminSudo}
+                                onChange={onScopesChange}
                             />
                             <label
                                 className="form-check-label"
-                                htmlFor="user-settings-create-access-token-page__scope-user:all"
+                                htmlFor="user-settings-create-access-token-page__scope-site-admin:sudo"
                             >
-                                <strong>{AccessTokenScopes.UserAll}</strong> — Full control of all resources accessible
-                                to the user account
+                                <strong>{AccessTokenScopes.SiteAdminSudo}</strong> — Ability to perform any action as
+                                any other user
                             </label>
                         </div>
-                        {this.props.user.siteAdmin && (
-                            <div className="form-check">
-                                <input
-                                    className="form-check-input"
-                                    type="checkbox"
-                                    id="user-settings-create-access-token-page__scope-site-admin:sudo"
-                                    checked={this.state.scopes.includes(AccessTokenScopes.SiteAdminSudo)}
-                                    value={AccessTokenScopes.SiteAdminSudo}
-                                    onChange={this.onScopesChange}
-                                />
-                                <label
-                                    className="form-check-label"
-                                    htmlFor="user-settings-create-access-token-page__scope-site-admin:sudo"
-                                >
-                                    <strong>{AccessTokenScopes.SiteAdminSudo}</strong> — Ability to perform any action
-                                    as any other user
-                                </label>
-                            </div>
-                        )}
-                    </div>
-                    <button
-                        type="submit"
-                        disabled={this.state.creationOrError === 'loading'}
-                        className="btn btn-success e2e-create-access-token-submit"
-                    >
-                        {this.state.creationOrError === 'loading' ? (
-                            <LoadingSpinner className="icon-inline" />
-                        ) : (
-                            <AddIcon className="icon-inline" />
-                        )}{' '}
-                        Generate token
-                    </button>
-                    <Link
-                        className="btn btn-secondary ml-1 e2e-create-access-token-cancel"
-                        to={this.props.match.url.replace(/\/new$/, '')}
-                    >
-                        Cancel
-                    </Link>
-                </Form>
-                {isErrorLike(this.state.creationOrError) && (
-                    <ErrorAlert
-                        className="invite-form__alert"
-                        error={this.state.creationOrError}
-                        history={this.props.history}
-                    />
-                )}
-            </div>
-        )
-    }
-
-    private onNoteChange: React.ChangeEventHandler<HTMLInputElement> = event =>
-        this.setState({ note: event.currentTarget.value })
-
-    private onScopesChange: React.ChangeEventHandler<HTMLInputElement> = event => {
-        const checked = event.currentTarget.checked
-        const value = event.currentTarget.value
-        this.setState(previousState => ({
-            scopes: checked ? [...previousState.scopes, value] : previousState.scopes.filter(scope => scope !== value),
-        }))
-    }
-
-    private onSubmit: React.FormEventHandler<HTMLFormElement> = event => this.submits.next(event)
+                    )}
+                </div>
+                <button
+                    type="submit"
+                    disabled={creationOrError === 'loading'}
+                    className="btn btn-success e2e-create-access-token-submit"
+                >
+                    {creationOrError === 'loading' ? (
+                        <LoadingSpinner className="icon-inline" />
+                    ) : (
+                        <AddIcon className="icon-inline" />
+                    )}{' '}
+                    Generate token
+                </button>
+                <Link
+                    className="btn btn-secondary ml-1 e2e-create-access-token-cancel"
+                    to={props.match.url.replace(/\/new$/, '')}
+                >
+                    Cancel
+                </Link>
+            </Form>
+            {isErrorLike(creationOrError) && (
+                <ErrorAlert className="invite-form__alert" error={creationOrError} history={props.history} />
+            )}
+        </div>
+    )
 }

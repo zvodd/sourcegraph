4250d7f7db90fc449737017aee4469babc8b0d21
Rafal Gajdulewicz <rafal.gajdulewicz@sourcegraph.com>
2022-03-09 17:15:21 +0100

    Force

--- client/web/src/user/settings/repositories/SettingsRepositoriesPage.tsx
+++ client/web/src/user/settings/repositories/SettingsRepositoriesPage.tsx
@@ -330,7 +330,7 @@ export const SettingsRepositoriesPage: React.FunctionComponent<Props> = ({
                     orgDisplayName={owner.name}
                 />
             )}
-            {!isUserOwner && authenticatedUser && org && org.viewerNeedsCodeHostUpdate && org.displayName && (
+            {!isUserOwner && authenticatedUser && org && org.displayName && (
                 <OrgUserNeedsGithubUpgrade user={authenticatedUser} orgDisplayName={org.displayName} />
             )}
             <PageTitle title="Your repositories" />

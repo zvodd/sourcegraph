520a4432bb158993b5abdf2e1a11543413caa3f3
Rafal Gajdulewicz <rafal.gajdulewicz@sourcegraph.com>
2022-03-10 14:50:42 +0100

    Update in place + use alerty

--- client/web/src/user/settings/codeHosts/UserAddCodeHostsPage.tsx
+++ client/web/src/user/settings/codeHosts/UserAddCodeHostsPage.tsx
@@ -44,6 +44,14 @@ type Status = undefined | 'loading' | ServicesByKind | ErrorLike
 const isServicesByKind = (status: Status): status is ServicesByKind =>
     typeof status === 'object' && Object.keys(status).every(key => keyExistsIn(key, ExternalServiceKind))
 
+export const updateGitHubApp = (): void => {
+    window.location.assign(
+        `/.auth/github/login?pc=${encodeURIComponent(
+            `https://github.com/::${window.context.githubAppCloudClientID}`
+        )}&op=createCodeHostConnection&redirect=${window.location.href}`
+    )
+}
+
 export const ifNotNavigated = (callback: () => void, waitMS: number = 2000): void => {
     let timeoutID = 0
     let willNavigate = false
@@ -332,11 +340,7 @@ export const UserAddCodeHostsPage: React.FunctionComponent<UserAddCodeHostsPageP
                         `${authProvider.authenticationURL as string}&redirect=${encodeURIComponent(firstRedirectURI)}`
                     )
                 } else {
-                    window.location.assign(
-                        `/.auth/github/login?pc=${encodeURIComponent(
-                            `https://github.com/::${window.context.githubAppCloudClientID}`
-                        )}&op=createCodeHostConnection&redirect=${window.location.href}`
-                    )
+                    updateGitHubApp()
                 }
             }
         },
